f7465b153f2c58411a7ea5dceccf5445696f3987
Pavithrakanchana <74247443+Pavithrakanchana@users.noreply.github.com>
2023-07-14 01:04:40 +0530

    header component completed

--- Task_Manager/src/app/header/header.module.ts
+++ Task_Manager/src/app/header/header.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 
 
@@ -9,7 +10,11 @@ import { HeaderComponent } from './header.component';
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class HeaderModule { }
