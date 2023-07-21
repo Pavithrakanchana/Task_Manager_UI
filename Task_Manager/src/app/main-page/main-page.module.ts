e3b42f2e924eb6b46cd0d2bf6855a175597063e2
Pavithrakanchana <74247443+Pavithrakanchana@users.noreply.github.com>
2023-07-21 22:39:08 +0530

    main page created

--- Task_Manager/src/app/main-page/main-page.module.ts
+++ Task_Manager/src/app/main-page/main-page.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatSidenavModule} from '@angular/material/sidenav';
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,11 @@ import {MatSidenavModule} from '@angular/material/sidenav';
     CommonModule,
     FormsModule, 
     ReactiveFormsModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule
   ],
   exports : [
     MainPageComponent
